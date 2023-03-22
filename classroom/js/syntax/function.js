80b170cbc5a22c510ea36691b0565a04537c17ba
Luiz <lucachaves@gmail.com>
2023-03-22 10:58:35 -0300

    função e módulos

--- classroom/js/syntax/function.js
+++ classroom/js/syntax/function.js
@@ -1,13 +1,63 @@
 // function declaration
+function sum(a, b) {
+  return a + b;
+}
 
 // anonymous/lambda function
+const minus = function (a, b) {
+  return a - b;
+};
 
 // arrow function
+const divide = (a, b) => {
+  return a / b;
+};
+
+const multiply = (a, b) => a * b;
+
+// call function
+console.log(sum(1, 1));
+console.log(sum(1));
+console.log(sum(1, 1, 1));
 
 // case sensitive
+function Sum(a, b) {
+  return `Soma: ${a + b}`;
+}
+
+console.log(Sum(1, 1));
+console.log(sum(1, 1));
 
 // function overriding
+function addition(a, b) {
+  return `Soma: ${a + b}`;
+}
+
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(1, 1));
 
 // default parameter
+function subtraction(a, b = 1) {
+  return a - b;
+}
+
+console.log(subtraction(10, 5));
+console.log(subtraction(10));
 
 // rest parameter
+function multiplication(...values) {
+  let result = 1;
+
+  for (const value of values) {
+    result *= value;
+  }
+
+  return result;
+}
+
+console.log(multiplication(1)); // [1]
+console.log(multiplication(1, 2)); // [1, 2]
+console.log(multiplication(1, 2, 3, 4)); // [1, 2 , 3 ,4]
---
+++ exercises/function-calc/calc.js
@@ -0,0 +1,19 @@
+/**
+ * operator: '+', '-', '*', '/'
+ */
+function calc(operand1, operand2, operator) {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    case '/':
+      return operand1 / operand2;
+    default:
+      return 'Invalid operator';
+  }
+}
+
+export { calc };
