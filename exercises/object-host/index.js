76038e06a48a1c131d29fb6d304655d6120b22b9
Luiz <lucachaves@gmail.com>
2023-04-02 23:22:39 -0300

    manipulação de objeto

---
+++ exercises/object-host/index.js
@@ -0,0 +1,15 @@
+// Host	Address	Mask
+// PC 1	192.168.0.1	255.255.255.0
+// Server	10.0.0.1	255.0.0.0
+const hosts = [
+  {
+    name: 'PC 1',
+    address: '192.168.0.1',
+    mask: '255.255.255.0',
+  },
+  {
+    name: 'Server',
+    address: '10.0.0.1',
+    mask: '255.0.0.0',
+  },
+];
