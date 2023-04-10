e3f3af03d16093cf34188036ab9f01b00b57248c
Luiz <lucachaves@gmail.com>
2023-04-10 11:39:37 -0300

    cadastro e remoção de hosts

---
+++ exercises/web-script-iptable/data/hosts.js
@@ -0,0 +1,20 @@
+export const hosts = [
+  {
+    id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+    ip: '192.168.0.1',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    id: '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed',
+    ip: '192.168.0.2',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+    ip: '192.168.0.3',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+];
--- exercises/web-script-iptable/js/main.js
+++ exercises/web-script-iptable/js/main.js
@@ -1,34 +1,64 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@iconify/iconify';
+import { v4 as uuidv4 } from 'uuid';
+import { hosts } from '../data/hosts.js';
 
-const hosts = [
-  {
-    ip: '192.168.0.1',
-    mask: '255.255.255.0',
-    version: 'v4',
-  },
-  {
-    ip: '192.168.0.2',
-    mask: '255.255.255.0',
-    version: 'v4',
-  },
-  {
-    ip: '192.168.0.3',
-    mask: '255.255.255.0',
-    version: 'v4',
-  },
-];
+function createHostView(host) {
+  const tbody = document.querySelector('tbody');
 
-const tbody = document.querySelector('tbody');
-
-let rows = '';
-
-for (const host of hosts) {
-  rows += `<tr>
+  const rowView = `<tr id="host-${host.id}">
     <td>${host.ip}</td>
     <td>${host.mask}</td>
     <td>${host.version}</td>
+    <td>
+      <div class="remove-host-icon">
+        <span class="iconify" data-icon="mdi:file-document-delete-outline"></span>
+      </div>
+    </td>
   </tr>`;
+
+  tbody.insertAdjacentHTML('beforeend', rowView);
+
+  const rowTag = document.querySelector(`tr#host-${host.id}`);
+
+  const deleteIcon = rowTag.querySelector('.remove-host-icon');
+
+  deleteIcon.onclick = function () {
+    rowTag.remove();
+  };
 }
 
-tbody.innerHTML = rows;
+function loadHostsView() {
+  for (const host of hosts) {
+    createHostView(host);
+  }
+}
+
+function loadSubmitHandler() {
+  const form = document.querySelector('form');
+
+  form.onsubmit = function (event) {
+    event.preventDefault();
+
+    const id = uuidv4();
+
+    const ip = document.querySelector('#ip').value;
+
+    const mask = document.querySelector('#mask').value;
+
+    const version = document.querySelector('input[name=version]:checked').value;
+
+    const host = { id, ip, mask, version };
+
+    createHostView(host);
+
+    form.reset();
+
+    document.querySelector('#close-offcanvas').click();
+  };
+}
+
+loadHostsView();
+
+loadSubmitHandler();
